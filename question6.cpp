5aa3ab211d114105459d98b74b9a2834c4723f50
Vikash Tiwari <95138490+vikashtiwari97@users.noreply.github.com>
2022-12-30 11:20:11 +0530

    Add files via upload

---
+++ question6.cpp
@@ -0,0 +1,32 @@
+//print alphabet
+#include<iostream>
+
+using namespace std;
+int main(){
+    int size=5,alphabet=65,number=0;
+    // upside pyramid
+    for(int i=1;i<=size;i++)
+    {
+        for(int j=size;j>i;j--)
+        {
+            cout<<" ";
+        }
+        for(int k=0;k<i*2-1;k++)
+        {
+            cout<<((char)(alphabet+number++));
+        }
+        number=0;
+        cout<<endl;
+    }
+    for(int i=1;i<=size-1;i++){
+        for(int j=0;j<i;j++){
+            cout<<" ";
+        }
+        for(int k=(size-i)*2-1;k>0;k--){
+            cout<<((char)(alphabet+number++));
+        }
+        number=0;
+        cout<<endl;
+    }
+    return 0;
+}
