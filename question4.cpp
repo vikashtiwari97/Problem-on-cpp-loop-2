38bd08195fc76343a9a45d9411608e3ae62c62da
Vikash Tiwari <95138490+vikashtiwari97@users.noreply.github.com>
2022-12-30 11:19:25 +0530

    Add files via upload

---
+++ question4.cpp
@@ -0,0 +1,18 @@
+// Write a program to reverse a given integer number
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int n, t, r, rev = 0;
+    cout << "Enter any number : ";
+    cin >> n;
+    t = n;
+    while (t > 0)
+    {
+        r = t % 10;
+        t = t / 10;
+        rev = rev * 10 + r;
+    }
+    cout << "Reverse of number " << n << " is " << rev;
+}
